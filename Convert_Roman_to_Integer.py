da4cfe6e0d1faea0d88e990fc301edb5eb94a563
hru003 <hru8181@gmail.com>
2022-12-07 11:48:18 +0530

    Update Convert_Roman_to_Integer.py

--- Convert_Roman_to_Integer.py
+++ Convert_Roman_to_Integer.py
@@ -1,6 +1,6 @@
-'''Input: s = "MCMXCIV"
+"""Input: s = "MCMXCIV"
 Output: 1994
-Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.'''
+Explanation: M = 1000, CM = 900, XC = 90 and IV = 4."""
 
 def romanToInt(s):
      """
