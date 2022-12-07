1cd0ea85ebea83bbc4a682e78fd618ae77086b0e
hru003 <hru8181@gmail.com>
2022-12-07 11:46:24 +0530

    Create Convert_Roman_to_Integer.py

---
+++ Convert_Roman_to_Integer.py
@@ -0,0 +1,24 @@
+'''Input: s = "MCMXCIV"
+Output: 1994
+Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.'''
+
+def romanToInt(s):
+     """
+     :type s: str
+     :rtype: int
+     """
+     d = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+     out = 0
+     for i in range(0,len(s)):
+          if i < len(s)-1:
+               if d[s[i]] >= d[s[i+1]]:
+                    out+=d[s[i]]
+               else:
+                    out = out - d[s[i]]
+          else:
+               out += d[s[i]]
+
+     return out
+
+s='MCMXCIV'
+romanToInt(s)
