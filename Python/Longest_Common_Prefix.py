e7479c7af03f51e34b049a2bb32e143a2225293e
hru003 <hru8181@gmail.com>
2022-12-07 08:52:31 +0000

    Formatted file

--- Python/Longest_Common_Prefix.py
+++ Python/Longest_Common_Prefix.py
@@ -4,22 +4,22 @@ Input: strs = ["flower","flow","flight"]
 Output: "fl"
 """
 
-strs = ["flower","flow","flight"]
+strs = ["flower", "flow", "flight"]
 
 m = ''
 flag = 1
 j = 0
 while flag == 1:
-     count = 0
-     temp = strs[0][j]
-     for i in range(0,len(strs)):
-          if strs[i][j] == temp:
-               count += 1
+    count = 0
+    temp = strs[0][j]
+    for i in range(0, len(strs)):
+        if strs[i][j] == temp:
+            count += 1
 
-     if count == len(strs):
-          j = j + 1
-     else:
-          flag = 0
+    if count == len(strs):
+        j = j + 1
+    else:
+        flag = 0
 
 m = strs[0][0:j]
 print(m)
