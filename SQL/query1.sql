cfd361e72db4c6d025009afd52dfdc6beeeeb3d1
hru003 <hru8181@gmail.com>
2022-12-07 13:41:37 +0000

    Query1

--- SQL/query1.sql
+++ SQL/query1.sql
@@ -1,2 +1,7 @@
--- SQLite
-CREATE TABLE name();
+CREATE TABLE contacts (
+	contact_id INTEGER PRIMARY KEY,
+	first_name TEXT NOT NULL,
+	last_name TEXT NOT NULL,
+	email TEXT NOT NULL UNIQUE,
+	phone TEXT NOT NULL UNIQUE
+);
