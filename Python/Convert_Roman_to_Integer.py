9c1026148d4119e58177d29390686c61db0d5efe
hru003 <hru8181@gmail.com>
2022-12-07 08:51:02 +0000

    Add print line to display the output

--- Python/Convert_Roman_to_Integer.py
+++ Python/Convert_Roman_to_Integer.py
@@ -2,23 +2,25 @@
 Output: 1994
 Explanation: M = 1000, CM = 900, XC = 90 and IV = 4."""
 
+
 def romanToInt(s):
-     """
-     :type s: str
-     :rtype: int
-     """
-     d = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
-     out = 0
-     for i in range(0,len(s)):
-          if i < len(s)-1:
-               if d[s[i]] >= d[s[i+1]]:
-                    out+=d[s[i]]
-               else:
-                    out = out - d[s[i]]
-          else:
-               out += d[s[i]]
+    """
+    :type s: str
+    :rtype: int
+    """
+    d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    out = 0
+    for i in range(0, len(s)):
+        if i < len(s)-1:
+            if d[s[i]] >= d[s[i+1]]:
+                out += d[s[i]]
+            else:
+                out = out - d[s[i]]
+        else:
+            out += d[s[i]]
 
-     return out
+    return out
 
-s='MCMXCIV'
-romanToInt(s)
+
+s = 'MCMXCIV'
+print(romanToInt(s))
